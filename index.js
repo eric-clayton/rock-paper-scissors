5b354bea0961dd22df5cfce581e938495ce64f2f
Eric Clayton <68301170+eric-clayton@users.noreply.github.com>
2024-08-06 21:16:59 -0400

    Create function and test for computer choice

--- index.js
+++ index.js
@@ -1,1 +1,31 @@
-console.log("Hello, World")
+// This function will randomly return rock paper or scissors
+function getComputerChoice(){
+    //Generate a random number 0 to 2
+    let choice = Math.floor(2*Math.random());
+    //return a string for each option corresoponding to each number
+    switch(choice) {
+        case 0:
+            return "rock";
+        case 1:
+            return "paper";
+        case 2:
+            return "scissors";
+        default:
+            console.error(`Invalid choice for computer: ${choice}`);
+    }
+}
+
+//Test getComputerChoice
+function testGetComputerChoice() {
+    for(let i = 0; i < 100; i++) {
+        try{
+            let answer = getComputerChoice();
+            console.assert( answer == "rock" || answer == "paper" || answer == "scissors", `Unexpected answer: ${answer}`);
+        }
+        catch(e) {
+            console.log()
+        }
+    }
+    console.log('testGetComputerChoice finished');
+}
+testGetComputerChoice();
